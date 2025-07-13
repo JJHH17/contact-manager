2c4ac423014b037f2f657d4b84accc8718001c41
James Hatfield <james.hatfield17@icloud.com>
2025-07-13 11:07:31 +0100

    Resolved typo in class name

--- src/main/java/userInterface/UserInterface.java
+++ src/main/java/userInterface/UserInterface.java
@@ -1,6 +1,6 @@
 package userInterface;
 
-public class UserIntergitface {
+public class UserInterface {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
