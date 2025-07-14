672e118c9202f3edb9b4867e8b4cc953c6d01855
James Hatfield <james.hatfield17@icloud.com>
2025-07-14 11:54:20 +0100

    Added code (with temp credentials) to test database connection

--- src/main/java/userInterface/UserInterface.java
+++ src/main/java/userInterface/UserInterface.java
@@ -1,7 +1,30 @@
 package userInterface;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class UserInterface {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        // Temporary credentials to test SQL connection
+        String url = "jdbc:postgresql://localhost:5432/ContactManager";
+        String username = "postgres";
+        String password = "";
+
+        // Establishing a connection
+        try {
+            Connection con = DriverManager.getConnection(url, username, password);
+            Statement st = con.createStatement();
+            // Where we execute query
+            // st.executeQuery();
+
+            con.close();
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+
+
     }
 }
