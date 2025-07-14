727ca575e7f37e182a9932ec50c6cc8a3060d4d6
James Hatfield <james.hatfield17@icloud.com>
2025-07-14 17:44:53 +0100

    Created method allowing us to create a new user from method

--- src/main/java/userInterface/UserInterface.java
+++ src/main/java/userInterface/UserInterface.java
@@ -8,7 +8,15 @@ public class UserInterface {
         // Authentication
         db.authenticate();
         db.createTable();
-        db.addUser("james.h@email.com", "James", "Hatfield");
-        db.addUser("Samantha@email.com", "Samantha", "P");
+    }
+
+    public void addUser(String email, String firstname, String lastname, int phoneNumber) {
+        // phone numbers cannot be > or < 8 characters
+        if (Integer.toString(phoneNumber).length() == 8) {
+            Contact newUser = new Contact(email, firstname, lastname, phoneNumber);
+            // Adds to DB
+        } else {
+            System.out.println("Please enter an 8 digit phone number");
+        }
     }
 }
