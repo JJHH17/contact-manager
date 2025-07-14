4304317792a1b61af632624a920deda7324aa179
James Hatfield <james.hatfield17@icloud.com>
2025-07-14 17:16:03 +0100

    Added database call to main method, added test data which successfully adds to the db

--- src/main/java/userInterface/UserInterface.java
+++ src/main/java/userInterface/UserInterface.java
@@ -9,41 +9,12 @@ import java.util.Properties;
 
 public class UserInterface {
     public static void main(String[] args) {
-        // Pulling from credential file
-        Properties props = new Properties();
 
-        try (InputStream input = new FileInputStream("db.properties")) {
-            props.load(input);
-        } catch (Exception e) {
-            System.out.print("An error occurred when pulling credentials");
-        }
-
-        // Temporary credentials to test SQL connection
-        String url = props.getProperty("db.url");
-        String username = props.getProperty("db.username");
-        String password = props.getProperty("db.password");
-
-        // Creating an example call to create a table if it doesnt exist
-        String sql = "CREATE TABLE IF NOT EXISTS contactManager (" +
-                "id SERIAL PRIMARY KEY, " +
-                "email CHAR NOT NULL, " +
-                "firstname CHAR NOT NULL, " +
-                "lastname CHAR NOT NULL);";
-
-        // Establishing a connection
-        try {
-            Connection con = DriverManager.getConnection(url, username, password);
-            Statement st = con.createStatement();
-            // Where we execute query
-            st.execute(sql);
-            System.out.println("Table created (if it didn't already exist!)");
-
-            // Closing the connection is extremely important...
-            con.close();
-        } catch (Exception e) {
-            System.out.println("Error");
-        }
-
-
+        Database db = new Database();
+        // Authentication
+        db.authenticate();
+        db.createTable();
+        db.addUser("james.h@email.com", "James", "Hatfield");
+        db.addUser("Samantha@email.com", "Samantha", "P");
     }
 }
