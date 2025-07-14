ef09d697992f6d03e7e00308be9d38d2b5e97346
James Hatfield <james.hatfield17@icloud.com>
2025-07-14 16:11:05 +0100

    Added postgresql connection with main file (to be refactored), pulls credentials from .properties file

--- src/main/java/userInterface/UserInterface.java
+++ src/main/java/userInterface/UserInterface.java
@@ -1,17 +1,27 @@
 package userInterface;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class UserInterface {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Pulling from credential file
+        Properties props = new Properties();
+
+        try (InputStream input = new FileInputStream("db.properties")) {
+            props.load(input);
+        } catch (Exception e) {
+            System.out.print("An error occurred when pulling credentials");
+        }
 
         // Temporary credentials to test SQL connection
-        String url = "jdbc:postgresql://localhost:5432/ContactManager";
-        String username = "postgres";
-        String password = "";
+        String url = props.getProperty("db.url");
+        String username = props.getProperty("db.username");
+        String password = props.getProperty("db.password");
 
         // Establishing a connection
         try {
@@ -20,6 +30,7 @@ public class UserInterface {
             // Where we execute query
             // st.executeQuery();
 
+            // Closing the connection is extremely important...
             con.close();
         } catch (Exception e) {
             System.out.println("Error");
