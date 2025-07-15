78ad80e9e71d153c523a7a14ab77d5c3f2fef82c
James Hatfield <james.hatfield17@icloud.com>
2025-07-15 11:38:29 +0100

    Split main program loop into seperate method, added exit criteria for loop

--- src/main/java/userInterface/UserInterface.java
+++ src/main/java/userInterface/UserInterface.java
@@ -1,32 +1,73 @@
 package userInterface;
 
+import java.util.Scanner;
 
 public class UserInterface {
+
+    private Database db = new Database();
+
+
     public static void main(String[] args) {
-
-        Database db = new Database();
-        // Authentication
-        db.authenticate();
-        db.createTable();
-
-
+        UserInterface ui = new UserInterface();
+        ui.start();
     }
 
+
+    public void start() {
+        Scanner input = new Scanner(System.in);
+        // Authentication and creation of Postgresql table
+        this.db.authenticate();
+        this.db.createTable();
+
+        while (true) {
+            System.out.println("'quit' to exit program");
+            System.out.println("Enter an email address");
+            String emailAddress = input.nextLine();
+
+            if (emailAddress.equalsIgnoreCase("quit")) {
+                break;
+            }
+
+            System.out.println("Enter a first name");
+            String firstName = input.nextLine();
+
+            if (firstName.equalsIgnoreCase("quit")) {
+                break;
+            }
+
+            System.out.println("Enter an last name");
+            String lastName = input.nextLine();
+
+            if (lastName.equalsIgnoreCase("quit")) {
+                break;
+            }
+
+            System.out.println("Enter a telephone number (max 8 characters)");
+            // Parses to string initially then converted after (once we've checked exit criteria)
+            String telephoneNumber = input.nextLine();
+            // TODO: Add data type validation here (when split into method)
+
+            if (telephoneNumber.equalsIgnoreCase("quit")) {
+                break;
+            }
+
+            this.addUser(emailAddress, firstName, lastName, Integer.parseInt(telephoneNumber));
+        }
+        input.close();
+    }
+
+
     public void addUser(String email, String firstname, String lastname, int phoneNumber) {
-        // phone numbers cannot be > or < 8 characters
-        Database db = databaseReturn();
+        // TODO - Add check for duplicate items; rejects entry if they exist
 
         if (Integer.toString(phoneNumber).length() == 8) {
             Contact newUser = new Contact(email, firstname, lastname, phoneNumber);
             // Adds to DB
-            db.addUser(email, firstname, lastname, phoneNumber);
+            this.db.addUser(email, firstname, lastname, phoneNumber);
 
         } else {
             System.out.println("Please enter an 8 digit phone number");
         }
     }
 
-    public Database databaseReturn() {
-        return new Database();
-    }
 }
