4a4213cf8dfa7ff729de64f223fb2753906fda91
James Hatfield <james.hatfield17@icloud.com>
2025-07-15 11:04:16 +0100

    Created method to create new DB object; added to addUser method, adds user to database

--- src/main/java/userInterface/UserInterface.java
+++ src/main/java/userInterface/UserInterface.java
@@ -8,15 +8,25 @@ public class UserInterface {
         // Authentication
         db.authenticate();
         db.createTable();
+
+
     }
 
     public void addUser(String email, String firstname, String lastname, int phoneNumber) {
         // phone numbers cannot be > or < 8 characters
+        Database db = databaseReturn();
+
         if (Integer.toString(phoneNumber).length() == 8) {
             Contact newUser = new Contact(email, firstname, lastname, phoneNumber);
             // Adds to DB
+            db.addUser(email, firstname, lastname, phoneNumber);
+
         } else {
             System.out.println("Please enter an 8 digit phone number");
         }
+    }
+
+    public Database databaseReturn() {
+        return new Database();
     }
 }
